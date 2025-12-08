233e51abcedccb4281b2fcc21585e4c9ce367875
Igor <appdeveloper@bk.ru>
2025-12-08 13:27:44 +0300

    Дополнил раздел ingress в values для helm

--- Project_template.md
+++ Project_template.md
@@ -341,7 +341,7 @@ kubectl delete  namespace cinemaabyss
 ```
 Запустите 
 ```bash
-helm install cinemaabyss .\src\kubernetes\helm --namespace cinemaabyss --create-namespace
+helm install cinemaabyss ./src/kubernetes/helm --namespace cinemaabyss --create-namespace
 ```
 Если в процессе будет ошибка
 ```code
--- src/microservices/proxy/cinema.apigateway/obj/Debug/net8.0/cinema.apigateway.AssemblyInfo.cs
+++ src/microservices/proxy/cinema.apigateway/obj/Debug/net8.0/cinema.apigateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cinema.apigateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5103fbc8546495cfe9d3aed3ac39ff1414b55e05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7f999584fc2b61866a5f7b3129a7e75f924ff2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("cinema.apigateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cinema.apigateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
